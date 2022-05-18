a4e6e54b9d078ced00a4922a8aed14f433b6c007
Vladislav <vladislavgrinev.sc@gmail.com>
2022-05-18 19:09:28 +0300

    minor

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.getElementById("copy-btn").addEventListener("click", () => {
+document.getElementById("copy-btn").addEventListener("click", e => {
+    e.target.blur();
     navigator.clipboard.writeText(document.getElementById("copy-box").value)
+
 })
 
-document.getElementById("paste-btn").addEventListener("click", () => {
+document.getElementById("paste-btn").addEventListener("click", e => {
+    e.target.blur();
     navigator.clipboard.readText().then(
         clipText => document.getElementById("paste-box").value = clipText
     )
