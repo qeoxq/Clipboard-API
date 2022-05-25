16f6c4c08e8c20af7f67728a909399bc51fad513
Vladislav <vladislavgrinev.sc@gmail.com>
2022-05-25 15:21:57 +0300

    Added picture

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ document.getElementById("paste-btn").addEventListener("click", e => {
         clipText => document.getElementById("paste-box").value = clipText
     )
 })
+
+document.getElementById("copy-picture-btn").addEventListener("click", e => {
+    e.target.blur();
+    fetch(document.getElementById("copy-picture").src)
+        .then(response => response.blob())
+        .then(data => {
+            navigator.clipboard.write([
+                new ClipboardItem({
+                    'image/png': data
+                })
+            ])
+        })
+})
+
+document.getElementById("paste-picture-btn").addEventListener("click", e => {
+    e.target.blur();
+    navigator.clipboard.read().then(
+        data => {
+            data[0].getType("image/png")
+                .then(blob => {
+                    document.getElementById("paste-picture").src = URL.createObjectURL(blob);
+                })
+                .catch(() => {
+                    alert("Clipboard contains non-image data.");
+                })
+        }
+    )
+})
